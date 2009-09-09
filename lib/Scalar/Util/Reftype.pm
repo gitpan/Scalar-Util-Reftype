755c8b8aee7c7a089f599a9070df0c80d641de02
Burak Gürsoy <burak@cpan.org>
2009-09-09 03:43:54 +0000

    Import of BURAK/Scalar-Util-Reftype-0.40 from CPAN.
    gitpan-cpan-distribution: Scalar-Util-Reftype
    gitpan-cpan-version:      0.40
    gitpan-cpan-path:         BURAK/Scalar-Util-Reftype-0.40.tar.gz
    gitpan-cpan-author:       BURAK
    gitpan-cpan-maturity:     released

--- lib/Scalar/Util/Reftype.pm
+++ lib/Scalar/Util/Reftype.pm
@@ -1,7 +1,12 @@
 package Scalar::Util::Reftype;
 use strict;
 use vars qw( $VERSION @ISA $OID @EXPORT @EXPORT_OK );
-use constant PRIMITIVES => qw(  Regexp IO SCALAR ARRAY HASH CODE GLOB REF );
+use constant HAS_FORMAT_REF => $] >= 5.008; # old ones don't have it
+use constant PRIMITIVES     => qw(
+    Regexp IO SCALAR ARRAY HASH CODE GLOB REF LVALUE
+),
+( HAS_FORMAT_REF ? qw(FORMAT) : () )
+;
 use subs qw( container class reftype type blessed object );
 use overload bool     => '_bool',
              fallback => 1,
@@ -10,10 +15,10 @@ use re           ();
 use Scalar::Util ();
 use Exporter     ();
 
-$VERSION = '0.31';
+$VERSION = '0.40';
 @ISA       = qw( Exporter );
 @EXPORT    = qw( reftype  );
-@EXPORT_OK = qw( type     );
+@EXPORT_OK = qw( type  HAS_FORMAT_REF );
 
 BEGIN {
     $OID = -1;
@@ -52,9 +57,9 @@ BEGIN {
     }
 }
 
-sub reftype { __PACKAGE__->new->analyze( @_ ) }
+sub reftype { __PACKAGE__->_new->_analyze( @_ ) }
 
-sub new {
+sub _new {
     my $class = shift;
     my $self  = [ map { 0 } 0..MAXID ];
     $self->[CONTAINER] = '';
@@ -62,7 +67,7 @@ sub new {
     return $self;
 }
 
-sub analyze {
+sub _analyze {
     my $self  = shift;
     my $thing = shift || return $self;
     my $ref   = CORE::ref($thing) || return $self;
@@ -143,8 +148,8 @@ Scalar::Util::Reftype - Alternate reftype() interface
 
 =head1 DESCRIPTION
 
-This document describes version C<0.31> of C<Scalar::Util::Reftype>
-released on C<3 September 2009>.
+This document describes version C<0.40> of C<Scalar::Util::Reftype>
+released on C<9 September 2009>.
 
 This is an alternate interface to C<Scalar::Util>'s C<reftype> function.
 Instead of manual type checking you can just call methods on the result
@@ -154,11 +159,31 @@ to see if matches the desired type.
 
 =head2 reftype EXPR
 
-Exported by default.
+Exported by default. C<EXPR> can be any value (even C<undef>).
 
 Returns an object with which you can call various test methods. Unless
 specified otherwise, all of the test methods return either zero (false)
 or one (true) based on the C<EXPR> you have specified.
+
+Return values of reftype() can not be used in boolean contexts. If you do,
+it'll die with a verbose error message.
+
+   my $r = reftype( $foo ) || 'something'; # dies
+   bar() if reftype( $foo );               # dies
+
+Always call the test methods on the return value:
+
+   bar() if reftype( $foo )->array;
+
+Or, if you want to have multiple tests, without executing C<reftype> multiple
+times:
+
+   my $r = reftype( $foo );
+   bar() if $r->array;
+   baz() if $r->array_object;
+   die "ooooh! scaaaary..." if $r->format_object;
+
+The available test methods are listed below.
 
 =head3 scalar
 
@@ -179,6 +204,14 @@ Tests if C<EXPR> is a CODE reference or not.
 =head3 glob
 
 Tests if C<EXPR> is a GLOB reference or not.
+
+=head3 lvalue
+
+Tests if C<EXPR> is a LVALUE reference or not.
+
+=head3 format
+
+Tests if C<EXPR> is a FORMAT reference or not.
 
 =head3 ref
 
@@ -215,6 +248,14 @@ Tests if C<EXPR> is a CODE reference based object or not.
 
 Tests if C<EXPR> is a GLOB reference based object or not.
 
+=head3 lvalue_object
+
+Tests if C<EXPR> is a LVALUE reference based object or not.
+
+=head3 format_object
+
+Tests if C<EXPR> is a FORMAT reference based object or not.
+
 =head3 ref_object
 
 Tests if C<EXPR> is a reference to a reference based object or not.
@@ -235,16 +276,11 @@ Tests if C<EXPR> is a Regexp reference based object or not.
 Returns the name of the class the object based on if C<EXPR> is an object.
 Returns an empty string otherwise.
 
-=head1 METHODS
-
-The module uses an OO backend which you won't be needing. Please use the
-C<reftype> function.
-
-=head2 new
-
-=head2 analyze EXPR
-
 =head1 CAVEATS
+
+=over 4
+
+=item *
 
 perl versions 5.10 and newer includes the function C<re::is_regexp> to detect
 if a reference is a regex or not. While it is possible to detect normal regexen
@@ -255,14 +291,52 @@ automated tool --like cpan shell-- will install it automatically)
 C<Data::Dump::Streamer> which provides the C<regex> function similar to
 C<re::is_regexp>.
 
+=item *
+
 IO refs are already implemented as objects, so both C<< reftype(EXPR)->io >>
 and C<< reftype(EXPR)->io_object >> will return true is C<EXPR> is either
 and IO reference or an IO reference based object.
 
+=item *
+
+C<VSTRING> references are not supported and not implemented.
+
+=item *
+
+C<FORMAT> references can be detected under perl 5.8 and newer. Under older
+perls, even the accessors are not defined for C<FORMAT>.
+
+=back
+
 =head1 SEE ALSO
 
-L<Scalar::Util>, L<Data::Dump::Streamer>, L<re>,
-L<http://perlmonks.org/?node_id=665339>.
+=over 4
+
+=item *
+
+C<reftype> in L<Scalar::Util>
+
+=item *
+
+L<Data::Dump::Streamer>
+
+=item *
+
+L<re>
+
+=item *
+
+L<http://perlmonks.org/?node_id=665339>
+
+=item *
+
+C<t/op/ref.t> in perl source
+
+=item *
+
+C<ref> in L<perlfunc>.
+
+=back
 
 =head1 AUTHOR
 
--- t/02-basic.t
+++ t/02-basic.t
@@ -1,35 +1,41 @@
 #!/usr/bin/env perl -w
 use strict;
 use Test::More qw( no_plan );
-use Scalar::Util::Reftype;
+use Scalar::Util::Reftype qw( reftype HAS_FORMAT_REF );
 
 my $scalar;
 my $sref    = \$scalar;
 my $ioref   = *STDIN{IO};
 my $re      = qr/ Testing /xmsi;
+my $gref    = \*STDOUT;
+my $lvalue  = \substr('',0);
 
 # normal refs
-is( reftype( []    )->array    , 1, 'Testing ARRAY'  );
-is( reftype( {}    )->hash     , 1, 'Testing HASH'   );
-is( reftype( \$0   )->scalar   , 1, 'Testing SCALAR' );
-is( reftype( sub{} )->code     , 1, 'Testing CODE'   );
-is( reftype(\$sref )->ref      , 1, 'Testing REF'    );
-is( reftype( $re   )->regexp   , 1, 'Testing Regexp' );
-is( reftype($ioref )->io       , 1, 'Testing IO'     );
-is( reftype($ioref )->io_object, 1, 'Testing IO'     );
-is( reftype( []    )->hash     , 0, 'Testing hash   on ARRAY' );
-is( reftype( {}    )->array    , 0, 'Testing array  on HASH'  );
-is( reftype( \$0   )->code     , 0, 'Testing code   on SCALAR');
-is( reftype( sub{} )->scalar   , 0, 'Testing scalar on CODE'  );
+is( reftype( \$0     )->scalar   , 1, 'Testing SCALAR' );
+is( reftype( []      )->array    , 1, 'Testing ARRAY'  );
+is( reftype( {}      )->hash     , 1, 'Testing HASH'   );
+is( reftype( sub{}   )->code     , 1, 'Testing CODE'   );
+is( reftype($gref    )->glob     , 1, 'Testing GLOB'   );
+is( reftype(\$sref   )->ref      , 1, 'Testing REF'    );
+is( reftype($ioref   )->io       , 1, 'Testing IO'     );
+is( reftype($ioref   )->io_object, 1, 'Testing IO'     );
+is( reftype( $re     )->regexp   , 1, 'Testing Regexp' );
+is( reftype( $lvalue )->lvalue   , 1, 'Testing LVALUE' );
+
+is( reftype( []      )->hash     , 0, 'Testing hash   on ARRAY' );
+is( reftype( {}      )->array    , 0, 'Testing array  on HASH'  );
+is( reftype( \$0     )->code     , 0, 'Testing code   on SCALAR');
+is( reftype( sub{}   )->scalar   , 0, 'Testing scalar on CODE'  );
 
 my $scalaro = bless $sref   , 'Foo';
 my $arrayo  = bless []      , 'Foo';
 my $hasho   = bless {}      , 'Foo';
 my $codeo   = bless sub {}  , 'Foo';
-my $globo   = bless \*STDOUT, 'Foo';
+my $globo   = bless $gref, 'Foo';
 my $refo    = bless \$sref  , 'Foo';
 my $ioo     = bless $ioref  , 'Foo';
 my $regexpo = bless $re     , 'Foo';
+my $lvalueo = bless $lvalue , 'Foo';
 
 # blessed refs
 is( reftype( $scalaro )->scalar_object,     1, 'Object is a  SCALAR object' );
@@ -40,7 +46,8 @@ is( reftype( $globo   )->glob_object,       1, 'Object is a  GLOB   object' );
 is( reftype( $refo    )->ref_object,        1, 'Object is a  REF    object' );
 is( reftype( $ioo     )->io_object,         1, 'Object is a  IO object'     );
 is( reftype( $ioo     )->io,                1, 'Object is a  IO object'     );
-is( reftype( $regexpo )->regexp_object,     1, 'Object is a  REGEXP object' );
+is( reftype( $regexpo )->regexp_object,     1, 'Object is a  Regexp object' );
+is( reftype( $lvalueo )->lvalue_object,     1, 'Object is a  LVALUE object' );
 
 is( reftype( $scalaro )->container    , 'Foo', 'Object is an instance of Foo (container)' );
 is( reftype( $scalaro )->class        , 'Foo', 'Object is an instance of Foo (class)' );
@@ -64,5 +71,15 @@ like(
     $@ => qr{can not be used in boolean contexts}ms,
     'Objects can not be used in boolean contexts'
 );
+
+SKIP: {
+    skip("Skipping FORMAT tests under old perl",2) if ! HAS_FORMAT_REF;
+    format STDERR =
+.
+    my $fref = *STDERR{FORMAT};
+    is( reftype( $fref )->format       , 1, 'Testing FORMAT' );
+    my $fobj = bless $fref, 'Foo';
+    is( reftype( $fobj )->format_object, 1, 'Object is a FORMAT object' );
+}
 
 __END__
